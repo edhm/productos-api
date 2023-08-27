e3762d0cd53f54888916608cc4b79c2dd79a2fda
Edhm <ed.huerta.m@gmail.com>
2023-08-27 12:17:54 -0500

    Implemented Product Entity

--- src/main/java/com/edhm/productos/api/repositories/ProductoRepository.java
+++ src/main/java/com/edhm/productos/api/repositories/ProductoRepository.java
@@ -2,11 +2,20 @@ package com.edhm.productos.api.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.edhm.productos.api.entities.Producto;
 
-public interface ProductoRepository extends CrudRepository<Producto, Long>{
-@Override
-List<Producto> findAll();
+public interface ProductoRepository extends CrudRepository<Producto, Long> {
+	@Override
+	List<Producto> findAll();
+	@Query("SELECT p FROM Producto p WHERE p.nombre LIKE %:name%")
+	List<Producto> searchByNameLike(@Param("name") String name);
+
+	/*
+@Query("SELECT p FROM productos p WHERE p.nombre LIKE %:name%")
+	List<Producto> searchByNameLike(@Param("name") String name);
+	 * */
 }
